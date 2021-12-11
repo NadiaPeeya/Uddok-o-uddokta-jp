e952102ba8eddce2a1011a38fbe861f3263181f6
NadiaPeeya <nadiapeeya05@gmail.com>
2021-12-11 15:50:40 +0600

    changes

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -30,44 +30,45 @@ const Footer = () => {
     <Col xs>
         <div className="footer-styling">
            
-            <h2 className="fw-bolder text-black">Quick Link</h2>
+            <h2 className="fw-bolder text-color-change">Quick Link</h2>
             {/* <div className=''>
                 <img src="" alt="" />
             </div> */}
-            <p className="fw-bold text-black"><Nav.Link className="text-black" as={HashLink} to="/home#aboutUs"> <span className=''>ABOUT US</span></Nav.Link></p>
-            <p className="fw-bold text-black"></p>
-            <p className="fw-bold text-black"><Nav.Link className="text-black" as={HashLink} to="/home#someCauses"> <span className=''>CAUSES</span></Nav.Link></p>
-            <p className="fw-bold text-black"> <Nav.Link className="text-black" as={HashLink} to="/home#mission"><span className=''>MiSSION</span></Nav.Link></p>
-            <p className="fw-bold text-black"><Nav.Link className="text-black" as={HashLink} to="/home#joinUs"> <span className='text-black'>JOIN US</span></Nav.Link></p>
-            <p className="fw-bold text-black"><Nav.Link className="text-black" as={HashLink} to="/home#joinUs"> <span className='text-black'>Contact</span></Nav.Link></p>
+            <p className="fw-bold text-color-change"><Nav.Link className="text-color-change" as={HashLink} to="/home#aboutUs"> <span className=''>ABOUT US</span></Nav.Link></p>
+            <p className="fw-bold text-color-change"></p>
+            <p className="fw-bold text-color-change"><Nav.Link className="text-color-change" as={HashLink} to="/home#someCauses"> <span className=''>CAUSES</span></Nav.Link></p>
+            <p className="fw-bold text-color-change"> <Nav.Link className="text-color-change" as={HashLink} to="/home#mission"><span className=''>MiSSION</span></Nav.Link></p>
+            
+            <p className="fw-bold text-color-change"><Nav.Link className="text-color-change" as={HashLink} to="/home#joinUs"> <span className='text-color-change'>JOIN US</span></Nav.Link></p>
+            <p className="fw-bold text-color-change"><Nav.Link className="text-color-change" as={HashLink} to="/home#joinUs"> <span className='text-color-change'>Contact</span></Nav.Link></p>
         </div>
     </Col>
     
     <Col xs={{ order: 12 }} className="footer-styling">
         <div className=''><img width="30%" src={logo} alt="" /></div>
-        {/* <h2 className="fw-bolder text-black"> Uddok O Uddokta </h2> */}
-        <p className="fw-bold text-black">Shinjuku Park Tower N30F,</p> 
-        <p className="fw-bold text-black">
+        {/* <h2 className="fw-bolder text-color-change"> Uddok O Uddokta </h2> */}
+        <p className="fw-bold text-color-change">Shinjuku Park Tower N30F,</p> 
+        <p className="fw-bold text-color-change">
         3-7-1 Nishishinjuku,Shinjuku-ku,
         </p >
         
-<p className="fw-bold text-black">Tokyo 163-1030, Japan</p>
+<p className="fw-bold text-color-change">Tokyo 163-1030, Japan</p>
 
-        <p className="fw-bold text-black">info@uddogouddokta.jp</p>
-        <p className="fw-bold text-black">+81-80-9666-8105</p>
+        <p className="fw-bold text-color-change">info@uddogouddokta.jp</p>
+        <p className="fw-bold text-color-change">+81-80-9666-8105</p>
     </Col>
     <Col xs={{ order: 1 }}>
-        <h2 className="fw-bolder text-black">Socials</h2>
-        <p className="fw-bold text-black fs-2"><a className='tag-link' target="_blank" href="https://www.facebook.com/uddogouddoktajapan"><i class="fab fa-facebook"></i></a> </p>
-        <p className="fw-bold text-black fs-2"><i class="fab fa-instagram-square"></i></p>
-        <p className="fw-bold text-black fs-2"><i class="fab fa-linkedin"></i></p>
-        <p className="fw-bold text-black fs-2"><i class="fab fa-twitter-square"></i></p>
+        <h2 className="fw-bolder text-color-change">Socials</h2>
+        <p className="fw-bold facebook fs-2"><i class="fab fa-facebook"></i> </p>
+        <p className="fw-bold text-color-change fs-2"><i class="fab fa-instagram-square"></i></p>
+        <p className="fw-bold text-color-change fs-2"><i class="fab fa-linkedin"></i></p>
+        <p className="fw-bold text-color-change fs-2"><i class="fab fa-twitter-square"></i></p>
     </Col>
   </Row>
 
 </Container>
 <div className="mt-5">
-<p className="text-black footer-bottom">Copyright <img width="1%" src="https://o.remove.bg/downloads/d1ae6f8a-5594-4bc7-86f0-45961b852fa7/001_2688246-5b916f32c9e77c0025c0cc9c-removebg-preview.png" alt="" /> 2021. All Rights Reserved</p>
+<p className="text-color-change footer-bottom">Copyright <img width="1%" src="https://o.remove.bg/downloads/d1ae6f8a-5594-4bc7-86f0-45961b852fa7/001_2688246-5b916f32c9e77c0025c0cc9c-removebg-preview.png" alt="" /> 2021. All Rights Reserved</p>
 </div>
      
         </div>
