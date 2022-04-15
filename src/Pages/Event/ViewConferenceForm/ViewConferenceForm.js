6d072c180c46820be0dde9caac670b4e21598b60
NadiaPeeya <nadiapeeya05@gmail.com>
2022-04-15 19:58:34 +0600

    updated form

--- src/Pages/Event/ViewConferenceForm/ViewConferenceForm.js
+++ src/Pages/Event/ViewConferenceForm/ViewConferenceForm.js
@@ -7,7 +7,7 @@ const ViewConferenceForm = () => {
         <div>
             <Top></Top>
             <Navigation></Navigation>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeIxnnQWcUdM9pG4bHrJ1mqGSebHScl5OP50IiPWdkjWnDkXw/viewform" className='donor-form' width="100%" height="2454" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeIxnnQWcUdM9pG4bHrJ1mqGSebHScl5OP50IiPWdkjWnDkXw/viewform?embedded=true" className='donor-form' width="100%" height="2454" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
         </div>
     );
