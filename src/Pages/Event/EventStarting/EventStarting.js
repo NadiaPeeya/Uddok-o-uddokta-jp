a735f93ed7003ada298a0929bed8dd8c267ea0b8
NadiaPeeya <nadiapeeya05@gmail.com>
2022-06-06 20:06:54 +0600

    updated

--- src/Pages/Event/EventStarting/EventStarting.js
+++ src/Pages/Event/EventStarting/EventStarting.js
@@ -16,19 +16,20 @@ const EventStarting = () => {
                     <p className='text-coloring'>July 02, 2022 | Saturday | 11:00 to 18:30 JST | Online</p>
             </div>
         <div className="row container m-5">
-               <Grid xs={12} sm={12} md={3} className="session1  container">
+               {/* <Grid xs={12} sm={12} md={4} className="session1  container">
                 
                <img width="100%" src="https://munimpact.org/wp-content/uploads/2020/06/Screenshot-2020-06-14-at-8.45.06-PM-1.png" alt="" />
         
-        </Grid>
-               <Grid xs={12} sm={12} md={3} className="session1 p-3 container">
-               <img width='100%' src="https://i.ibb.co/NnnxVQr/Logo-1.png" alt="" />
+        </Grid> */}
+         <Grid xs={12} sm={12} md={3} className="session1 p-3 container">
+               <img width="80%" src="https://i.ibb.co/FYWYqSz/image-27-12-21-03-53.jpg" alt="" />
         
         </Grid>
                <Grid xs={12} sm={12} md={3} className="session1 p-3 container">
-               <img width="100%" src="https://i.ibb.co/FYWYqSz/image-27-12-21-03-53.jpg" alt="" />
+               <img width='80%' src="https://i.ibb.co/NnnxVQr/Logo-1.png" alt="" />
         
         </Grid>
+              
                <Grid xs={12} sm={12} md={3} className="session1 p-3 container">
                <img width="100%" src="https://1000logos.net/wp-content/uploads/2017/06/Toshiba-logo.jpg" alt="" />
         
